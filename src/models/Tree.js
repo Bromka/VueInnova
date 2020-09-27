257b0cfa1837686bf3c3a58705674fb88e845d10
Bromka <efimov.iliya@gmail.com>
2020-09-27 09:53:50 +0300

    sync

--- src/models/Tree.js
+++ src/models/Tree.js
@@ -1,5 +1,5 @@
 import Commentary from "./Commentary";
-// import getRandomInt from "./Utils";
+import getRandomInt from "./Utils";
 
 export default class Tree {
     #data;
@@ -11,24 +11,28 @@ export default class Tree {
             children: [{comment: 'Lorem2'}, {comment: 'Lorem3'}, {comment: 'Lorem4', children: [{comment: 'Lorem5'}]}]
         }, {
             comment: 'Lorem11',
-            children: [{comment: 'Lorem21'}, {comment: 'Lorem31'}, {comment: 'Lorem41', children: [{comment: 'Lorem51'}]}]
+            children: [{comment: 'Lorem21'}, {comment: 'Lorem31'}, {
+                comment: 'Lorem41',
+                children: [{comment: 'Lorem51'}]
+            }]
         }]
         this.createTree(10)
     }
 
-    get Data(){
+    get Data() {
         return this.data
     }
-    set Data(data){
+
+    set Data(data) {
         this.data = data
     }
 
-    createComment(author, text){
-        let comment = new Commentary (author, text)
+    createComment(author, text) {
+        let comment = new Commentary(author, text)
         return comment
     }
 
-    async fetchData(url){
+    async fetchData(url) {
         // let a = await fetch(`https://jsonplaceholder.typicode.com/comments/1`)
         let a = await fetch(url)
         let k = await a.json()
@@ -37,14 +41,45 @@ export default class Tree {
 
     }
 
-    async createTree(numberOfComments = 10){
+    createTree(numberOfComments = 10) {
         let a = [];
-        for (let i=0; i<numberOfComments; i++){
-            this.fetchData(`https://jsonplaceholder.typicode.com/comments/${i+1}`)
-            .then(json =>  this.createComment(json.email,  json.body))
-            .then(com => {a.push({id: i, comment: com})})
+        let par;
+        let Promises = []
+        for (let i = 0; i < numberOfComments; i++) {
+            let Promise = this.fetchData(`https://jsonplaceholder.typicode.com/comments/${i + 1}`)
+                .then(json => this.createComment(json.email, json.body))
+                .then(com => {
+                    if (getRandomInt(3) % 3 !== 0 && i > 1) {
+                        par = getRandomInt(i - 1)
+                    } else {
+                        par = null
+                    }
+                    a.push({
+                        id: i, comment: com, parent: par
+                    })
+                })
+            Promises.push(Promise)
         }
-        console.log(a);
+        Promise.all(Promises).then(() => {
+            console.log(a)
+            // eslint-disable-next-line no-unused-vars
+            let testTree = [];
+            a.forEach((el) => {
+                if (el.parent !== null) {
+                    let d = testTree.find((item) => {
+                        console.log(item)
+                        item.id == el.parent
+                    })
+                    console.log(d)
+                } else {
+                    testTree.push({comment: el.comment, children: [], id: el.id})
+                }
+            })
+
+            console.log(testTree)
+        })
+
+
         this.data = a;
     }
 }
